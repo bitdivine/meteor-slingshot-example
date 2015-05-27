2f6c0c9eabee8d923108bae7f09f9099db9b34c7
Max Murphy-Skvorzov <mmurphy@telemetry.com>
2015-05-27 23:35:14 +0100

    First working upload :-)

--- uploader.js
+++ uploader.js
@@ -4,42 +4,38 @@ Slingshot.fileRestrictions("myFileUploads", {
 });
 
 if (Meteor.isClient) {
-	var uploader = new Slingshot.Upload("myFileUploads");
-
-	uploader.send(document.getElementById('input').files[0], function (error, downloadUrl) {
-	  if (error) {
-	    // Log service detailed response
-	    console.error('Error uploading', uploader.xhr.response);
-	    alert (error);
-	  }
-	  else {
-	    Meteor.users.update(Meteor.userId(), {$push: {"profile.files": downloadUrl}});
-	  }
-	});
-
-	Template.progressBar.helpers({
-	  progress: function () {
-	    return Math.round(this.uploader.progress() * 100);
-	  }
-	});
+  Template.uploader.events(
+  { 'click button': function () {
+      var uploader = new Slingshot.Upload("myFileUploads");
+      uploader.send(document.getElementById('slingshot_upload').files[0], function (error, downloadUrl) {
+        if (error) {
+          // Log service detailed response
+          console.error('Error uploading'); //, uploader.xhr.response);
+          alert (error);
+        } else {
+          Meteor.users.update(Meteor.userId(), {$push: {"profile.files": downloadUrl}});
+        }
+      });
+    }
+  });
 }
 if (Meteor.isServer) {
 	Slingshot.createDirective("myFileUploads", Slingshot.S3Storage, {
-	  bucket: "demteam",
-
+	  bucket: Meteor.settings.AWSBucket,
+	  region: Meteor.settings.AWSRegion,
 	  acl: "public-read",
 
 	  authorize: function () {
+	    return true;
 	    //Deny uploads if user is not logged in.
 	    if (!this.userId) {
 	      var message = "Please login before posting files";
 	      throw new Meteor.Error("Login Required", message);
 	    }
-
-	    return true;
 	  },
 
 	  key: function (file) {
+            return "the-one-and-only/" + file.name;
 	    //Store file into a directory by the user's username.
 	    var user = Meteor.users.findOne(this.userId);
 	    return user.username + "/" + file.name;
